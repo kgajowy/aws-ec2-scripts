0c9b52a30e7d54b14b10887259cef3bd9ecf465a
Kamil Gajowy <kgajowy@users.noreply.github.com>
2019-03-13 15:44:04 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install --latest
-nvm use --latest
+nvm install --lts
+nvm use --lts
 sudo apt install nginx -y
 }
