310f465a677a552a68cf1c71fe64d1b0215387d9
Kamil Gajowy <kgajowy@users.noreply.github.com>
2019-03-13 15:42:07 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,5 +10,8 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.33.3/install.sh | bash
 nvm install --latest
 nvm use --latest
 sudo apt install nginx -y
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 }
