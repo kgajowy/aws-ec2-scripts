34eabd996517025d2a865b2cea8ee22dab274d92
Kamil Gajowy <kgajowy@users.noreply.github.com>
2019-04-05 20:14:16 +0200

    Update nvm installation version

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt update -y
 # sudo apt upgrade -y
 sudo apt-get update -y
 sudo apt-get install build-essential libssl-dev -y
-curl https://raw.githubusercontent.com/creationix/nvm/v0.33.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
